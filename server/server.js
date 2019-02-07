50f1d5dd25eea3b2e0d76f3a8f4070e073ed1575
khengschew <khengc@gmail.com>
2019-02-07 11:02:37 -0800

    Complete full stack setup

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+
+const app = express();
+const port = process.env.PORT || 3040;
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+app.use(morgan('default'));
+
+app.listen(port, () => { console.log(`Listening on port ${port}`); });
