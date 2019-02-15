66d427c28d591653c8771e59ae129ededcc89f9e
khengschew <khengc@gmail.com>
2019-02-15 08:05:33 -0800

    Update articles styling to center titles, truncate when necessary

--- client/src/components/gtk/gTKArticleItem.jsx
+++ client/src/components/gtk/gTKArticleItem.jsx
@@ -5,10 +5,12 @@ const GTKArticleItem = (props) => {
 
   const articleDiv = (
     <div className="GTKArticleItem">
-      <a href={`/${id}`}>
-        <div className="GTKArticleImage" style={{ backgroundImage: `url(${article.image})` }} />
-        <div className="GTKArticleText">{article.name}</div>
-      </a>
+      <div className="GTKArticleImage" style={{ backgroundImage: `url(${article.image})` }} />
+      <div className="GTKArticleTextContainer">
+        <div className="GTKArticleText">
+          {article.name}
+        </div>
+      </div>
     </div>
   );
 
